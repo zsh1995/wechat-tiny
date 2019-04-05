8bf250d3a15ffa7230b3154af209782be497e682
mrzsh <453452586@qq.com>
2019-04-05 17:14:54 +0800

    temp

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,8 @@ var qcloud = require('../../bower_components/wafer-client-sdk/index');
 // 引入配置
 var config = require('../../config');
 var utils= require('../../utils/util.js')
+let parseHtml = require('../../utils/richTextParse/richText.js')
+
 
 var picUrls = {
   'rcmd-star-1': 'https://s1.ax1x.com/2018/03/09/9Rk1oV.jpg',
@@ -16,7 +18,26 @@ var picUrls = {
 }
 
 //
-var introdoce = "• 帮助广大青年提高情商和逆商、团队协作、工作和恋爱、婚姻质量；\n• 帮助大学生提高就业竞争力；\n• LG电子新员工培训，做出“很实用”的结论；\n• 清华土木系学生会建议学生认真体验！";
+var introdoce = "<h4>1、求职/工作/恋爱关键能力</h4>\
+\
+<ul><li>思维能力（判断/解决问题）</li><li>沟通能力（表达/理解/共识）</li></ul>\
+\
+<h4>2、思维与沟通训练</h4>\
+\
+<ul><li>这里提供很多事例和观点。</li><li>自己思考，与恋人、朋友交流，加深爱情、友情。</li></ul>\
+\
+<h4>3、成功案例</h4>\
+\
+<ul><li>LG电子新员工培训，半年后问卷调查反馈说很实用！之后，该公司与我们订了正式协议，承诺优先面试我们推荐的人（所有企业都可复制）！</li><li>华夏幸福基业新员工培训，反馈说“对公司增加了理解。”</li></ul>\
+\
+<h4>4、星级学员益处</h4>\
+\
+<ul><li>求职、找对象很容易证明自己思维很积极，沟通能力很好。</li><li>可安排名优企面试，详情请点击“我”。</li></ul>\
+\
+<h4>5、愿景</h4>\
+\
+<ul><li>推动全国高校和职教开设“思维与沟通”课为必修课。</li><li>清华已将“写作与沟通训练”设为必修课</li><li>思维与语言沟通是更基本的能力，全国高校开设“思维与沟通训练”课是必然趋势！</li><li>我们模式与内容是绝佳方案！</li><li>下一步推动所有年轻员工补此必修课！所有年轻员工补此必修课！</li></ul>\
+";
 var slogan = '致力于积极教育\n（Positiveness Education）\n的社会企业';
 var floatText = '清华x-lab培育'
 
@@ -31,6 +52,7 @@ Page({
     requestUserInfo: `https://${config.service.host}//ajax/user/detailInfo`,
     slogan: slogan,
     introdoce: introdoce,
+    height: '100',
   },
 
   //
@@ -143,9 +165,30 @@ Page({
     }
   },
 
+  scrollHeight() {
+    let that = this
+    const query = wx.createSelectorQuery()
+    query.select('#up').boundingClientRect()
+    query.select('#down').boundingClientRect()
+    query.selectViewport()
+    query.exec(function (res) {
+      console.log(res[0].bottom)
+      console.log(res[1].top)
+      let delta = res[1].top - res[0].bottom
+      that.setData({
+        height: delta
+      })
+    })
+
+  },
+  onReady(e) {
+    this.scrollHeight()
+  },
+
   onLoad: function (e) {
     console.log(e)
     console.log('onLoad')
+    introdoce = parseHtml.go(introdoce)
     var that = this
     wx.setStorage({
       key: 'picUrls',
